4b7f2513aff92b698e0edd948eb90431d85cfa5e
mohammed <m2008m1033m@gmail.com>
2019-01-02 11:25:32 +1100

    init

---
+++ deploy.js
@@ -0,0 +1,22 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+    'metal only cage twin dignity invite fire peace help wasp father treat',
+    'https://rinkeby.infura.io/v3/04dd5fe55e1d405983ea00cd2fb43fe1'
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    console.log('Attempting to deploye from account', accounts[0]);
+
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: ['Hi there!'] })
+        .send({ from: accounts[0], gas: '2000000' });
+
+    console.log('Contract deployed to', result.options.address);
+};
+deploy();
